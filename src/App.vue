e9280a6742144fa7a2ae1e6bff398ab1f94cffca
chogyejin <whrpwls96@naver.com>
2022-03-21 22:06:07 +0900

    feat: Add Conditionals and Loops

--- src/App.vue
+++ src/App.vue
@@ -2,55 +2,27 @@
 export default {
   data() {
     return {
-      message: "Hello World!",
-      time: new Date().toLocaleString(),
+      show: true,
+      list: [{ number: 1 }, { number: 2 }, { number: 3 }],
     };
   },
   methods: {
-    splitString() {
-      if (typeof this.message !== "string") return;
-      this.message = this.message.split(""); // string to array
-    },
-    reverseArray() {
-      if (!Array.isArray(this.message)) return;
-      this.message = this.message.reverse(); // reverse array
-    },
-    joinArray() {
-      if (!Array.isArray(this.message)) return;
-      this.message = this.message.join(""); // array to string
-    },
-    alertMessage() {
-      alert("안녕");
-    },
-    appendBang() {
-      if (Array.isArray(this.message)) return;
-      this.message += "!";
-    },
-    notify() {
-      alert("navigation was prevented.");
+    stopPush() {
+      console.log("12312");
     },
   },
 };
 </script>
 
 <template>
-  <div>
-    <span v-bind:title="time">
-      여기에 마우스를 올려두고 잠시 기다리면 제목이 동적으로 바뀝니다!
-    </span>
-  </div>
-  <div>
-    <h1>{{ message }}</h1>
-    <input v-model="message" />
-  </div>
-  <div><button @click="splitString">문자열을 배열로</button></div>
-  <div><button @click="reverseArray">배열 뒤집기</button></div>
-  <div><button @click="joinArray">배열을 문자열로</button></div>
-  <div><button @click="alertMessage">안녕</button></div>
-  <div><button @click="appendBang">Append "!"</button></div>
-  <div>
-    <a href="https://vuejs.org" @click.prevent="notify">
-      A link with e.preventDefault()
-    </a>
-  </div>
+  <button @click="show = !show">show 토글</button>
+  <button @click="list.push({ number: list.length + 1 })">숫자 push</button>
+  <button @click="list.pop()">숫자 pop</button>
+  <button @click="list.reverse()">뒤집기</button>
+
+  <ul v-if="show && list.length">
+    <li v-for="(item, index) of list" v-bind:key="index">{{ item.number }}</li>
+  </ul>
+  <p v-else-if="list.length">숫자는 있으니까 show 토글 켜요</p>
+  <p v-else>숫자 없음</p>
 </template>
